f9471428faa39b8b2dad7785095b0af6b2c5a8d3
snerks <martin.github@outlook.com>
2020-04-17 15:31:10 +0100

    Updates

---
+++ precache-manifest.2a2f60028d7ba29287e130b07089b5a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76749d6573689ae0b31fa15f48db8051",
+    "url": "/react-mui-reversi-ts/index.html"
+  },
+  {
+    "revision": "f0b1b5ee48524362c78c",
+    "url": "/react-mui-reversi-ts/static/css/main.01a5ae9e.chunk.css"
+  },
+  {
+    "revision": "ff6130e4c610af4aff06",
+    "url": "/react-mui-reversi-ts/static/js/2.c73808c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-mui-reversi-ts/static/js/2.c73808c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b1b5ee48524362c78c",
+    "url": "/react-mui-reversi-ts/static/js/main.752aab93.chunk.js"
+  },
+  {
+    "revision": "d705b389f36ee1cf843b",
+    "url": "/react-mui-reversi-ts/static/js/runtime-main.5dd7ca8b.js"
+  }
+]);

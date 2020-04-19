8bd49a6b108c09d826604024d6398914d133eca3
snerks <martin.github@outlook.com>
2020-04-19 15:12:05 +0100

    Updates

--- precache-manifest.da89e93f38b8cf1a40632140336d09b1.js
+++ precache-manifest.da89e93f38b8cf1a40632140336d09b1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d7eab9fe3b540cef63066f9c7ffe29f3",
+    "revision": "9498f20431e6e73fbb7ea9906002ff76",
     "url": "/react-mui-reversi-ts/index.html"
   },
   {
-    "revision": "5f63173e60e2c5701441",
+    "revision": "237499b773efe3d344f2",
     "url": "/react-mui-reversi-ts/static/css/main.d6f94b6f.chunk.css"
   },
   {
-    "revision": "694530319378a555b49c",
-    "url": "/react-mui-reversi-ts/static/js/2.a6a1213b.chunk.js"
+    "revision": "365c13a3585eb66c6295",
+    "url": "/react-mui-reversi-ts/static/js/2.4743dc70.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/react-mui-reversi-ts/static/js/2.a6a1213b.chunk.js.LICENSE.txt"
+    "url": "/react-mui-reversi-ts/static/js/2.4743dc70.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5f63173e60e2c5701441",
-    "url": "/react-mui-reversi-ts/static/js/main.7a98b28f.chunk.js"
+    "revision": "237499b773efe3d344f2",
+    "url": "/react-mui-reversi-ts/static/js/main.3165f29f.chunk.js"
   },
   {
     "revision": "d705b389f36ee1cf843b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-mui-reversi-ts/precache-manifest.ed41e4d5921e8e7a0ee4d1f887024eca.js"
+  "/react-mui-reversi-ts/precache-manifest.da89e93f38b8cf1a40632140336d09b1.js"
 );
 
 self.addEventListener('message', (event) => {
